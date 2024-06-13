8530d6776ae38493dfe37a320b8474828050e7ca
jeffzh4ng <zhang.jeffreyd@gmail.com>
2024-06-13 12:14:06 -0400

    Rename compiler to autograd

--- autograd/src/engine.rs
+++ autograd/src/engine.rs
@@ -12,6 +12,18 @@ pub enum Op {
 pub struct Expr {
     pub evaluated: NotNan<f32>,
     pub expr: Option<(Op, BTreeSet<Box<Expr>>)>,
+    pub grad: NotNan<f32>,
+    pub backward: (),
+}
+
+impl Expr {
+    pub fn new(evaluated: NotNan<f32>, expr: Option<(Op, BTreeSet<Box<Expr>>)>) -> Self {
+        Expr {
+            evaluated,
+            expr,
+            grad: NotNan::new(0.0).unwrap(),
+        }
+    }
 }
 
 impl Add for Expr {
@@ -26,6 +38,7 @@ impl Add for Expr {
         Expr {
             evaluated,
             expr: Some((Op::Add, operands)),
+            grad: todo!(),
         }
     }
 }
@@ -42,6 +55,7 @@ impl Mul for Expr {
         Expr {
             evaluated,
             expr: Some((Op::Mul, operands)),
+            grad: todo!(),
         }
     }
 }
